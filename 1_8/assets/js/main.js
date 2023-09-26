f9760fcd8fdc29f1701f3651a9c2718827247172
Andre Wolf <Andre77242@hotmail.de>
2023-09-26 11:38:27 +0200

    added exercise 1_8

---
+++ 1_8/assets/js/main.js
@@ -0,0 +1,4 @@
+const reiseziele = ["niederland", "schweden", "dänemark", "Ungarn", "Rumänien"];
+console.log(reiseziele);
+let slicedReiseziele = reiseziele.slice(2, 4);
+console.log(slicedReiseziele);
